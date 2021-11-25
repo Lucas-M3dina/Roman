61492d7d533b94d625619634ec493f3b321f2f6c
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2021-11-25 11:19:38 -0300

    Iniciando banco de dados

---
+++ Banco de dados/Scripts/dql.sql
@@ -0,0 +1,14 @@
+USE ROMAN;
+GO
+
+INSERT INTO professor(nomeProfessor)
+VALUES (''),(''), ('');
+GO
+
+INSERT INTO tema(nomeTema)
+VALUES (''),(''),('');
+GO
+
+INSERT INTO projeto(idTema, idProfessor, nomeProjeto, descricao)
+VALUES (''),(''),('');
+GO
