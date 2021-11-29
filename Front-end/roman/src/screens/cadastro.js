3b487981b98546dc170e9e97b50cf9be20527436
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2021-11-29 10:35:18 -0300

    Iniciando tela de listagem de projetos

---
+++ Front-end/roman/src/screens/cadastro.js
@@ -0,0 +1,180 @@
+/* import React, {Component} from 'react';
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
+
+import api from '../services/api';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export default class Cadastro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listaProjetos: [],
+    };
+  }
+
+  Inscrever = async idProjeto => {
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+
+      console.warn(idevento);
+
+      await api.post(
+        '/Projetos/listaPro/' + idProjeto,
+        {},
+        {
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+        },
+      );
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  // define a função que faz a chamada para a API e traz os eventos
+  buscarProjetos = async () => {
+    // define uma constante pra receber a resposta da requisição
+    const resposta = await api.get('/Projetos');
+    // com a função console.warn() é possível mostrar alertas na tela do dispositivo mobile
+    // console.warn(resposta.data[0])
+    // recebe o corpo da resposta
+    const dadosDaApi = resposta.data;
+    // atualiza o state listaEventos com este corpo da requisição
+    this.setState({listaProjetos: dadosDaApi});
+  };
+
+  render() {
+    return (
+      <View style={styles.main}>
+        Cabeçalho - Header
+        <View style={styles.mainHeader}>
+          <View style={styles.mainHeaderRow}>
+            <Image
+              source={require('../../assets/img/calendar.png')}
+              style={styles.mainHeaderImg}
+            />
+            <Text style={styles.mainHeaderText}>{'Projetos'.toUpperCase()}</Text>
+          </View>
+          <View style={styles.mainHeaderLine} />
+        </View>
+
+        Corpo - Body
+        <View style={styles.mainBody}>
+          <FlatList
+            contentContainerStyle={styles.mainBodyContent}
+            data={this.state.listaEventos}
+            keyExtractor={item => item.idProjeto}
+            renderItem={this.renderItem}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  renderItem = ({item}) => (
+    // <Text style={{ fontSize: 20, color: 'red' }}>{item.nomeEvento}</Text>
+
+    <View style={styles.flatItemRow}>
+      <View style={styles.flatItemImg}>
+        <TouchableOpacity
+          onPress={() => this.Inscrever(item.idProjeto)}
+          style={styles.flatItemImg}>
+          <Image
+            source={require('../../assets/img/view.png')}
+            style={styles.flatItemImgIcon}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  // unidades de medida
+
+  // porcentagem
+  // ex: height: '50%'
+
+  // px
+  // ex: height: 50 (não é necessário colocar a unidade de medida px)
+
+  // proporção
+  // ex: flex: 1
+
+  // conteúdo da main
+  main: {
+    flex: 1,
+    backgroundColor: '#F1F1F1',
+  },
+  // cabeçalho
+  mainHeader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainHeaderRow: {
+    flexDirection: 'row',
+  },
+  // imagem do cabeçalho
+  mainHeaderImg: {
+    width: 22,
+    height: 22,
+    tintColor: '#ccc',
+    marginRight: -5,
+    marginTop: -12,
+  },
+  // texto do cabeçalho
+  mainHeaderText: {
+    fontSize: 16,
+    letterSpacing: 5,
+    color: '#999',
+  },
+  // linha de separação do cabeçalho
+  mainHeaderLine: {
+    width: 220,
+    paddingTop: 10,
+    borderBottomColor: '#999',
+    borderBottomWidth: 1,
+  },
+
+  // conteúdo do body
+  mainBody: {
+    flex: 4,
+  },
+  // conteúdo da lista
+  mainBodyContent: {
+    paddingTop: 30,
+    paddingRight: 50,
+    paddingLeft: 50,
+  },
+  // dados do evento de cada item da lista (ou seja, cada linha da lista)
+  flatItemRow: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginTop: 40,
+  },
+  flatItemContainer: {
+    flex: 1,
+  },
+  flatItemTitle: {
+    fontSize: 16,
+    color: '#333',
+  },
+  flatItemInfo: {
+    fontSize: 12,
+    color: '#999',
+    lineHeight: 24,
+  },
+  flatItemImg: {
+    justifyContent: 'center',
+  },
+  flatItemImgIcon: {
+    width: 26,
+    height: 26,
+    tintColor: '#B727FF',
+  },
+}); */
