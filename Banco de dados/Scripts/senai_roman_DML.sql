39461f597af140c02978928fddf786f2c654a01f
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2021-11-29 10:33:41 -0300

    Adicionando professores

--- Banco de dados/Scripts/senai_roman_DML.sql
+++ Banco de dados/Scripts/senai_roman_DML.sql
@@ -11,15 +11,19 @@ GO
 
 INSERT INTO usuario(idTipoUsuario, email, senha)
 VALUES (1,'saulo@gmail.com','C# melhor que python'),
-       (1,'lucas@gmail.com','123');
+       (1,'lucas@gmail.com','123'),
+	   (2,'gustavo@gmail.com','1234'),
+       (2,'medina@gmail.com','1234');
 GO
 
 INSERT INTO professor(idUsuario, nomeProfessor)
 VALUES (1,'Saulo'),
-       (2,'Lucas');
+       (2,'Lucas'),
+	   (3, 'Gustavo'),
+	   (4, 'Lucas Medina');
 GO
 
 INSERT INTO projeto(idProfessor, idTema, nomeProjeto, descricao)
-VALUES (1, 1, 'Projeto de Controle de Estoque', 'Lista os personagens da DC e Marvel'),
-       (2, 2, 'Projeto de Listagem de Personagens', 'Faz o controle de estoque de uma loja');
+VALUES (3, 2, 'Projeto de Controle de Estoque do Senai', 'Faz o controle de estoque do senai'),
+       (4, 1, 'Projeto de Listagem de Personagens de series', 'Lista os personagens de series da netflix');
 GO
